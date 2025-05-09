508f1ce0cda89511541e7c654283d1ce15239983
angelo milella <angelo_milelladev@yahoo.it>
2025-05-09 22:57:42 +0200

    sistemazione scrip creazione board

--- project/utils/create_board.py
+++ project/utils/create_board.py
@@ -1,5 +1,6 @@
 import cv2
 import cv2.aruco as aruco
+import os
 
 # Definizione del dizionario ArUco
 aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
@@ -10,6 +11,11 @@ board = aruco.CharucoBoard((7, 5), 0.04, 0.03, aruco_dict)
 # Creazione dell'immagine della board
 img = board.generateImage((700, 500), marginSize=10, borderBits=1)
 
-# Salvataggio dell'immagine
-cv2.imwrite("../data/charuco_board.png", img)
+
+output_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../data"))
+os.makedirs(output_dir, exist_ok=True)  # crea ../data se non esiste
+
+output_path = os.path.join(output_dir, "charuco_board.png")
+cv2.imwrite(output_path, img)
+
 print("✅ Board salvata come 'charuco_board.png'")
